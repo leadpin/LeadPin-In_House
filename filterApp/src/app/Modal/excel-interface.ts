24b549d8aca78cf8cdf0adda42ef850cb8e815ce
leadpin <sutarsandeep1702@gmail.com>
2025-07-23 18:26:20 +0530

    mobile number column added

--- filterApp/src/app/Modal/excel-interface.ts
+++ filterApp/src/app/Modal/excel-interface.ts
@@ -1,10 +1,9 @@
 export interface IExcelData {
-  // reduce(arg0: (seen: any, obj: any) => any, arg1: {}): unknown;
-  // filter(arg0: (obj: any) => boolean): unknown;
   fullName: string;
   jobTitle: string;
   companyName: string;
   email: string;
+  mobileNumber: string;
   industry: string;
   employeeSize: string;
   phoneNumber: string;
