fb947f681a527fb7bc9f9c612f22cc55a799e818
leadpin <sutarsandeep1702@gmail.com>
2025-07-24 10:43:27 +0530

    clear button added for filter by company-name and daomain.

--- filterApp/src/app/components/upload-text-filter/upload-text-filter.component.ts
+++ filterApp/src/app/components/upload-text-filter/upload-text-filter.component.ts
@@ -1,9 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { dataClean } from '../../Modal/excel-functions';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-upload-text-filter',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './upload-text-filter.component.html',
   styleUrl: './upload-text-filter.component.scss',
 })
@@ -13,11 +21,20 @@ export class UploadTextFilterComponent {
   @Output() selectionChanged = new EventEmitter<string[]>();
   @Input() placeholder!: string;
   @Input() imageName!: string;
+  @ViewChild('textAreaName') myTextareaRef!: ElementRef<HTMLTextAreaElement>;
 
   onUploadData(event: Event): void {
     const input = (event.target as HTMLTextAreaElement).value;
     this.options = dataClean(input);
 
+    this.selectionChanged.emit(this.options);
+  }
+
+  clearData() {
+    if (this.myTextareaRef) {
+      this.myTextareaRef.nativeElement.value = '';
+    }
+    this.options = [];
     this.selectionChanged.emit(this.options);
   }
 }
